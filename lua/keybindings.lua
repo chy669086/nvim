e4ae716353ca288405487d92efa785bad98b2a09
Dynamic_Pigeon <3040313336@qq.com>
2025-01-21 22:41:51 +0800

    init

---
+++ lua/keybindings.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+local map = vim.api.nvim_set_keymap
+-- 复用 opt 参数
+local opt = { noremap = true, silent = true }
+
+-- bufferline
+-- 左右Tab切换
+map("n", "<A-h>", ":BufferLineCyclePrev<CR>", opt)
+map("n", "<A-l>", ":BufferLineCycleNext<CR>", opt)
+
+map("n", "<A-m>", ":NvimTreeToggle<CR>", opt)
